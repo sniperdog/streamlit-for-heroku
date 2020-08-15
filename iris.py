677c927b7bae9069401f5f9d9bcb2ebfe375b5bb
Gavin Webster <gw@exemail.com.au>
2020-08-15 14:19:34 +1000

    Classifier Example

--- iris.py
+++ iris.py
@@ -2,8 +2,15 @@ import streamlit as st
 
 def main():
 	""" A simple Iris EDA App """
-	st.title("Iris EDA App with streamlit")
-	st.subheader("Streamlit is Cool")
+	st.title("Streamlit example")
+	
+	st.write("""
+	# Explore different classifiers
+	Which one is the best?
+	""")
+
+	dataset_name = st.selectbox("Select dataset",("Iris","Breast Cancer","Wine dataset"))
+	st.write(dataset_name)
 
 if __name__ == '__main__':
 	main()
