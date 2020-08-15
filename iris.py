b5599e59b5c1bda83d3a40f0cbdd93eda2576bb6
Gavin Webster <gw@exemail.com.au>
2020-08-15 16:42:43 +1000

    add sliders

--- iris.py
+++ iris.py
@@ -1,5 +1,10 @@
 import streamlit as st
 from sklearn import datasets
+
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestClassifier
+
 import numpy as np
 
 def main():
@@ -29,6 +34,23 @@ def main():
 	X, y = get_dataset(dataset_name)
 	st.write("shape of dataset", X.shape)
 	st.write("number of classes", len(np.unique(y)))
+
+	def add_parameter_ui(clf_name):
+		params = dict()
+		if clf_name == "KNN":
+			K = st.slider("K",1,15)
+			params["K"] = K
+		elif clf_name == "SVM":
+			C = st.slider("C",0.01,10.0)
+			params["C"] = C
+		else:
+			max_depth = st.slider("max_depth", 2, 15)
+			n_estimators = st.slider("n_estimators", 1, 100)
+			params["max_depth"] = max_depth
+			params["n_estimators"] = n_estimators
+		return params
+
+	add_parameter_ui(classifier_name)
 
 if __name__ == '__main__':
 	main()
